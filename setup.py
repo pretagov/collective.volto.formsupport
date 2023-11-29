b04eed2c7a3895cb3759a9bba9910d8c8cb451d3
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2023-11-29 22:39:01 +0000

    Release 2.7.1.dev5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="collective.volto.formsupport",
-    version="2.7.1.dev4",
+    version="2.7.1.dev5",
     description="Add support for customizable forms in Volto",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
